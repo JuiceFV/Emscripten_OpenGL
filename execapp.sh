fdb8f9a37edb6480cc8e9ee30824c9d5ba24eef4
JuiceFV <aleksandr.juicefv@gmail.com>
2020-10-04 21:24:54 +0300

    added lunch script

---
+++ execapp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Compiling C++ code to the WASM. Writes them over './application/out/' directory"
+em++ /app/application/main.cxx -o /app/application/out/app.html
+echo "Runing the server. Follow the link http://localhost:8080"
+python3 /app/application/entry.py
