652b7f750d2d40ea262b75eadc9e5b9905462bdb
JuiceFV <aleksandr.juicefv@gmail.com>
2020-10-05 23:42:11 +0300

    cmake modified

--- cmake/Modules/graphics.cmake
+++ cmake/Modules/graphics.cmake
@@ -2,4 +2,5 @@ set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
 set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 
-add_subdirectory(${CMAKE_SOURCE_DIR}/dependencies/glfw-3.3.2)
+add_subdirectory(${CMAKE_SOURCE_DIR}/application/dependencies/GLFW)
+add_subdirectory(${CMAKE_SOURCE_DIR}/application/dependencies/GLEW)
