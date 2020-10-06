61b2a8be1697f243fba0a0c018ff7c7c3226d5f4
JuiceFV <aleksandr.juicefv@gmail.com>
2020-10-06 22:06:49 +0300

    yaml parser added

--- application/main.cxx
+++ application/main.cxx
@@ -1,4 +1,4 @@
-//emcc appication/main.cxx -o index.html -s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1 -std=c++1z
+// emcc application/main.cxx -I"application/dependencies/yamlprsr/include" -l"application/dependencies/build/libyaml-cpp.so" -o index.html -s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1 -std=c++1z
 #include <functional>
 #ifdef __EMSCRIPTEN__
     #define GLFW_INCLUDE_ES3
@@ -8,12 +8,21 @@
 #endif
 
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include "yaml-cpp/yaml.h"
 
 std::function<void()> loop;
 void main_loop() { loop(); }
 
 int main(void)
 {
-    
+    YAML::Node config = YAML::LoadFile("../config.yaml");
+
+if (config["lastLogin"]) { std::cout << "Ok"; }
+    else
+    {
+        std::cout << "Not Ok";
+    }
+
     return 0;
 }
