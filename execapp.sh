361ef6b96b2c5177878ca09693a79d3a0ea026ee
JuiceFV <aleksandr.juicefv@gmail.com>
2020-10-07 23:09:16 +0300

    the config has been fitted to the docker emcc proper build

--- execapp.sh
+++ execapp.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # generate yaml lib
+cd build
 echo "Compiling C++ code to the WASM. Writes them over './application/out/' directory"
-em++ /app/application/main.cxx -o /app/application/out/app.html
-echo "Runing the server. Follow the link http://localhost:8080"
-python3 /app/application/entry.py
+emcmake cmake -DFOR_EMSDK=ON ..
+emmake make
+node Emscripten_Graphics.js
+#echo "Runing the server. Follow the link http://localhost:8080"
+#python3 /app/application/entry.py
