17bff82a3ee99b7fb739fa2b6e972ba4273fd43f
JuiceFV <aleksandr.juicefv@gmail.com>
2020-10-08 21:03:42 +0300

    added global defaults

---
+++ application/configloader/includes/globals.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "config.h"
+
+namespace cfg
+{
+	namespace defaults
+	{
+		// The base window's width
+        constexpr unsigned int WIN_WIDTH = 640;
+
+        // The base window's height
+		constexpr unsigned int WIN_HEIGHT = 480;
+
+	}
+} // namespace cfg
