745e08ddd03c0889920deb38b313cea4404f135e
JuiceFV <aleksandr.juicefv@gmail.com>
2020-10-10 00:04:45 +0300

    rules for execute updated

--- execapp.sh
+++ execapp.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# generate yaml lib
+echo "Installing required packages..."
+pip3 install aiohttp
+pip3 install aiohttp_jinja2
+pip3 install jinja2
 cd build
 echo "Compiling C++ code to the WASM. Writes them over './application/out/' directory"
 emcmake cmake -DFOR_EMSDK=ON ..
 emmake make
-node Emscripten_Graphics.js
-#echo "Runing the server. Follow the link http://localhost:8080"
-#python3 /app/application/entry.py
+echo "Runing the server. Follow the link http://localhost:8080"
+python3 ./application/entry.py
