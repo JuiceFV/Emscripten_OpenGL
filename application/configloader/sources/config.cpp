2aec7d1850723d8c2769d3b4b512d891c27caee3
JuiceFV <aleksandr.juicefv@gmail.com>
2020-10-10 22:13:37 +0300

    added default config upload

--- application/configloader/sources/config.cpp
+++ application/configloader/sources/config.cpp
@@ -1,34 +1,22 @@
 #include "config.h"
 
-cfg::Config::Config()
+cfg::Config::Config() : is_default(false)
 {
-// A little explanation:
-// The problem appears when the code compiles via emcc.
-// Emscripten "mounts" its own file system in purpose to avoid
-// spare files. Actually, emscripten's team came up with the great idea.
-// Intending to achive the best performance and size - emcc incorporates
-// those files required by code. There are some ways to mount the host
-// volume whether through code or using flags. However, it's not demanded.
-// We may pass the required files, which are not appear in the code, via
-// the flag `--preload-file /form/where/@/to/where`. The problem is
-// that the default dir is the root "/". If we run the application
-// using non-emcc, to acquire the config.yaml we have to step back to
-// the previous directory and then move to config-directory (`../config/config.yaml).
-// Opposite to non-emcc the emcc sets the base directory as root one and then 
-// move in the config-directory (`/config/config.yaml`). 
-#if defined(__EMSCRIPTEN__)
-    this->Path = fs::current_path() /=
-#else
-    this->Path = fs::current_path().parent_path() /=
-#endif
-        "config/appconfig.yaml";
-    for (auto &p : fs::directory_iterator(this->getPath())) std::cout << p.path() << '\n';
+    this->Path = fs::current_path() /= "config/appconfig.yaml";
+    this->defineConfig(this->Path.string());
+}
+
+cfg::Config::Config(const std::string &path) : is_default(false)
+{
+    this->Path = path;
+    this->defineConfig(this->Path.string());
+}
+
+void cfg::Config::defineConfig(const std::string &path)
+{
     try
     {
-        if ((this->config = YAML::LoadFile(this->Path.string())["application"]))
-        {
-            this->win_size = this->parseWinSize();
-        }
+        if ((this->cfg_file = YAML::LoadFile(path)["application"])) { this->win_size = this->parseWinSize(); }
         else
         {
             throw std::invalid_argument("Missing 'application' parameter in 'appconfig.yaml'.");
@@ -38,40 +26,33 @@ cfg::Config::Config()
     {
         std::cerr << "An error has occured in the " << __FILE__ << " in line " << __LINE__ << "." << std::endl
                   << "Details: " << error.what() << std::endl
-                  << "You have to directly assign the application's settings." << std::endl;
-        // TODO may be add a default config if one was missing in config.yaml
-        exit(-1);
+                  << "You have to directly assign the application's settings." << std::endl
+                  << "The default settings will be uploaded, you may familiarize to them in the globals.h" << std::endl;
+        this->uploadDefaultCfg();
     }
 }
 
-cfg::Config::Config(const std::string &path) {}
-
 std::string cfg::Config::getPath() { return (this->Path.parent_path().string()); }
-
-// TODO create dynamical return of a property depends on passed enum arg
-auto cfg::Config::getWindowProp(){
-
-};
 
 // The private method which parses and returns width and heigth as dict.
 std::unordered_map<std::string, unsigned int> cfg::Config::parseWinSize()
 {
     std::unordered_map<std::string, unsigned int> res; // resulted map
-    unsigned int width = defaults::WIN_WIDTH;          // base width of window
-    unsigned int height = defaults::WIN_HEIGHT;        // base height of window
+    unsigned int width = defaults::WIN_WIDTH;          // default width of window
+    unsigned int height = defaults::WIN_HEIGHT;        // default height of window
 
     // Check if width/height or at least window parameter has been passed.
     // Otherwise, notify an user and set these parameters with default values.
     try
     {
-        if (this->config["window"])
+        if (this->cfg_file["window"])
         {
-            if (this->config["window"]["width"])
-                width = this->config["window"]["width"].as<unsigned int>();
+            if (this->cfg_file["window"]["width"])
+                width = this->cfg_file["window"]["width"].as<unsigned int>();
             else
                 throw std::invalid_argument("Missing 'window.width' parameter in 'appconfig.yaml'.");
-            if (this->config["window"]["height"])
-                height = this->config["window"]["height"].as<unsigned int>();
+            if (this->cfg_file["window"]["height"])
+                height = this->cfg_file["window"]["height"].as<unsigned int>();
             else
                 throw std::invalid_argument("Missing 'window.height' parameter in 'appconfig.yaml'.");
         }
@@ -91,5 +72,16 @@ std::unordered_map<std::string, unsigned int> cfg::Config::parseWinSize()
     res.insert({"height", height});
     return res;
 }
+
+void cfg::Config::uploadDefaultCfg()
+{
+    this->is_default = true;
+    this->Path = "";
+    this->cfg_file = NULL;
+    this->win_size.insert({"width", defaults::WIN_WIDTH});
+    this->win_size.insert({"height", defaults::WIN_HEIGHT});
+}
+
+inline bool cfg::Config::isConfigDefault() { return this->is_default; }
 
 cfg::Config::~Config() {}
