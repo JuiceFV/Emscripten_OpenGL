af2134bcee0feb4ee92ef61a64365b6b1afc3908
JuiceFV <aleksandr.juicefv@gmail.com>
2020-10-13 23:44:10 +0300

    gradient triangle, shaders trouble

--- application/main.cxx
+++ application/main.cxx
@@ -1,21 +1,191 @@
-// emcc application/main.cxx -I"application/dependencies/yamlprsr/include" -l"application/dependencies/build/libyaml-cpp.so" -o index.html -s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1 -std=c++1z
+// emcc application/main.cxx -I"application/dependencies/yamlprsr/include"
+// -l"application/dependencies/build/libyaml-cpp.so" -o index.html -s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1 -std=c++1z
 #include <functional>
 #ifdef __EMSCRIPTEN__
-    #define GLFW_INCLUDE_ES3
-    #include <emscripten/emscripten.h>
+#define GLFW_INCLUDE_ES3
+#include <emscripten/emscripten.h>
 #else
-    #include <GL/glew.h>
+#include <GL/glew.h>
 #endif
 
-#include <GLFW/glfw3.h>
-#include <iostream>
 #include "application.h"
+#include <GLFW/glfw3.h>
+#include <functional>
+#include <iostream>
 
-std::function<void()> loop;
-void main_loop() { loop(); }
-
-int main(void)
+#ifdef __EMSCRIPTEN__
+static void dispatch_main(void *fp)
 {
+    std::function<void()> *func = (std::function<void()> *)fp;
+    (*func)();
+}
+#endif
 
-    return 0;
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+// Shaders
+const GLchar *vertexShaderSource = "attribute vec4 position;                     \n"
+                                   "void main()                                  \n"
+                                   "{                                            \n"
+                                   "  gl_Position = vec4(position.xyz, 1.0);     \n"
+                                   "}                                            \n";
+const GLchar *fragmentShaderSource = "precision mediump float;\n"
+                                     "void main()                                  \n"
+                                     "{                                            \n"
+                                     "  gl_FragColor[0] = gl_FragCoord.x/640.0;    \n"
+                                     "  gl_FragColor[1] = gl_FragCoord.y/480.0;    \n"
+                                     "  gl_FragColor[2] = 0.5;                     \n"
+                                     "}                                            \n";
+int main()
+{
+    // Init GLFW
+    glfwInit();
+
+    // Set all the required options for GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // Create a GLFWwindow object that we can use for GLFW's functions
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+    if (nullptr == window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+#ifndef __EMSCRIPTEN__
+    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+    glewExperimental = GL_TRUE;
+    // Initialize GLEW to setup the OpenGL Function pointers
+    if (GLEW_OK != glewInit())
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return EXIT_FAILURE;
+    }
+#endif
+
+    // Define the viewport dimensions
+    glViewport(0, 0, screenWidth, screenHeight);
+
+    // Build and compile our shader program
+    // Vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Check for compile time errors
+    GLint success;
+    GLchar infoLog[512];
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Check for compile time errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Link shaders
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Set up vertex data (and buffer(s)) and attribute pointers
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f, // Left
+        0.5f,  -0.5f, 0.0f, // Right
+        0.0f,  0.5f,  0.0f  // Top
+    };
+
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as
+                                      // the currently bound vertex buffer object so afterwards we can safely unbind
+
+    glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+#ifdef __EMSCRIPTEN__
+    std::function<void()> mainLoop = [&]() {
+#else
+    while (!glfwWindowShouldClose(window))
+    {
+#endif
+        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response
+        // functions
+        glfwPollEvents();
+
+        // Render
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+#ifdef __EMSCRIPTEN__
+    };
+    emscripten_set_main_loop_arg(dispatch_main, &mainLoop, 0, 1);
+#else
+    }
+#endif
+
+    // Properly de-allocate all resources once they've outlived their purpose
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+    // Terminate GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
 }
