bc4aa7c6beaafc6d48b475c4081709b1f426f7e7
JuiceFV <aleksandr.juicefv@gmail.com>
2020-10-18 12:04:50 +0300

    shader class added, however required to be modified

--- application/includes/shader.h
+++ application/includes/shader.h
@@ -14,3 +14,13 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+
+class Shader
+{
+  public:
+    GLuint Program;
+    // Constructor generates the shader on the fly
+    Shader(const GLchar *vertexPath, const GLchar *fragmentPath);
+    // Uses the current shader
+    void Use();
+};
