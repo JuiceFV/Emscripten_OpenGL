028f9b8f388e4edcc4a6508a1a388a79f202ea94
JuiceFV <aleksandr.juicefv@gmail.com>
2020-10-29 19:14:45 +0300

    types modified

--- application/includes/camera.h
+++ application/includes/camera.h
@@ -26,11 +26,11 @@ enum Camera_Movement
 };
 
 // Default camera values
-const GLfloat YAW = -90.0f;
-const GLfloat PITCH = 0.0f;
-const GLfloat SPEED = 6.0f;
-const GLfloat SENSITIVTY = 0.25f;
-const GLfloat ZOOM = 45.0f;
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 6.0f;
+const float SENSITIVTY = 0.25f;
+const float ZOOM = 45.0f;
 
 // An abstract camera class that processes input and calculates the corresponding Eular Angles, Vectors and Matrices for
 // use in OpenGL
@@ -39,25 +39,25 @@ class Camera
   public:
     // Constructor with vectors
     Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
-           GLfloat yaw = YAW, GLfloat pitch = PITCH);
+           float yaw = YAW, float pitch = PITCH);
 
     // Constructor with scalar values
-    Camera(GLfloat posX, GLfloat posY, GLfloat posZ, GLfloat upX, GLfloat upY, GLfloat upZ, GLfloat yaw, GLfloat pitch);
+    Camera(float pos_x, float pos_y, float pos_z, float up_x, float up_y, float up_z, float yaw, float pitch);
 
     // Returns the view matrix calculated using Eular Angles and the LookAt Matrix
     glm::mat4 GetViewMatrix();
 
     // Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera
     // defined ENUM (to abstract it from windowing systems)
-    void ProcessKeyboard(Camera_Movement direction, GLfloat deltaTime);
+    void ProcessKeyboard(Camera_Movement direction, float delta_time);
 
     // Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
-    void ProcessMouseMovement(GLfloat xOffset, GLfloat yOffset, GLboolean constrainPitch = true);
+    void ProcessMouseMovement(float x_offset, float y_offset, unsigned char constrain_pitch = true);
 
     // Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
-    void ProcessMouseScroll(GLfloat yOffset);
+    void ProcessMouseScroll(float y_offset);
 
-    GLfloat GetZoom();
+    float GetZoom();
 
   private:
     // Camera Attributes
@@ -65,16 +65,17 @@ class Camera
     glm::vec3 front;
     glm::vec3 up;
     glm::vec3 right;
-    glm::vec3 worldUp;
+    glm::vec3 world_up;
 
     // Eular Angles
-    GLfloat yaw;
-    GLfloat pitch;
+    float yaw;
+    float pitch;
+    float roll;
 
     // Camera options
-    GLfloat movementSpeed;
-    GLfloat mouseSensitivity;
-    GLfloat zoom;
+    float movement_speed;
+    float mouse_sensitivity;
+    float zoom;
 
     // Calculates the front vector from the Camera's (updated) Eular Angles
     void updateCameraVectors();
