c70d3d48a432a71bf28c10104e8458744a7bcdcd
JuiceFV <aleksandr.juicefv@gmail.com>
2020-11-12 15:06:42 +0300

    cmake updated

--- cmake/Modules/graphics.cmake
+++ cmake/Modules/graphics.cmake
@@ -28,11 +28,28 @@ message(STATUS "SOIL2 - Done!\n")
 
 # Further I'ill decide if I shall to use the Assimp
 message(STATUS "Building the ASSIMP")
-option(ASSIMP_BUILD_ASSIMP_TOOLS OFF)
-option(ASSIMP_BUILD_TESTS OFF)
-option(ASSIMP_INSTALL OFF)
-set(BUILD_SHARED_LIBS ON CACHE BOOL "" FORCE)
-set(SYSTEM_IRRXML OFF CACHE BOOL "" FORCE)
-add_subdirectory(${CMAKE_SOURCE_DIR}/application/dependencies/ASSIMP)
+if (UNIX)
+find_library(assimp NAMES assimp libassimp.a
+			PATHS ${CMAKE_SOURCE_DIR}/application/dependencies/ASSIMP/build/code)	
+message(${assimp})
+else()
+find_library(assimp NAMES assimp-vc142-mtd.lib 
+			PATHS ${CMAKE_SOURCE_DIR}/application/dependencies/assimp_libs/lib/windows)
+	list(APPEND COMMON_LIBS ${assimp})
+	configure_file(${CMAKE_SOURCE_DIR}/application/dependencies/assimp_libs/lib/windows/assimp-vc142-mtd.dll 
+                        ${CMAKE_BINARY_DIR}/bin/Debug/assimp-vc142-mtd.dll
+                        COPYONLY
+                    )
+endif()
+if (assimp)
+message("Assimp has been found!")
+include_directories(${CMAKE_SOURCE_DIR}/application/dependencies/assimp_libs/include)
+endif()
+#set(ASSIMP_BUILD_ASSIMP_TOOLS OFF CACHE BOOL "" FORCE)
+#set(ASSIMP_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+#set(ASSIMP_INSTALL OFF CACHE BOOL "" FORCE)
+#set(BUILD_SHARED_LIBS ON CACHE BOOL "" FORCE)
+#set(SYSTEM_IRRXML OFF CACHE BOOL "" FORCE)
+#add_subdirectory(${CMAKE_SOURCE_DIR}/application/dependencies/ASSIMP)
 message(STATUS "ASSIMP - Done!\n")
 
