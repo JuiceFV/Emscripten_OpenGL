b0d541dbd766765067f16cddc98e069d669a3fb7
JuiceFV <aleksandr.juicefv@gmail.com>
2020-11-22 16:24:37 +0300

    added yamlparser copy dll

--- cmake/Modules/copydlls.cmake
+++ cmake/Modules/copydlls.cmake
@@ -3,18 +3,24 @@
     message(STATUS "Setting up copy glew dll..." "${CMAKE_BINARY_DIR}/bin/Debug/")
     add_custom_command(TARGET ${PROJECT_NAME}
     POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different 
-        "${CMAKE_SOURCE_DIR}/application/dependencies/glew/lib/msvc/glewd.dll"      
+        "${CMAKE_SOURCE_DIR}/application/dependencies/GLEW/lib/msvc/glewd.dll"      
         $<TARGET_FILE_DIR:${PROJECT_NAME}>)
 
     message(STATUS "Setting up copy glfw dll..." "${CMAKE_BINARY_DIR}/bin/Debug/")
     add_custom_command(TARGET ${PROJECT_NAME}
     POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different 
-        "${CMAKE_SOURCE_DIR}/application/dependencies/glfw/lib/msvc/glfw3.dll"      
+        "${CMAKE_SOURCE_DIR}/application/dependencies/GLFW/lib/msvc/glfw3.dll"      
         $<TARGET_FILE_DIR:${PROJECT_NAME}>)
 
     message(STATUS "Setting up copy assimp dll..." "${CMAKE_BINARY_DIR}/bin/Debug/")
     add_custom_command(TARGET ${PROJECT_NAME}
     POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different 
-        "${CMAKE_SOURCE_DIR}/application/dependencies/assimp/lib/msvc/assimp-vc142-mtd.dll"      
+        "${CMAKE_SOURCE_DIR}/application/dependencies/ASSIMP/lib/msvc/assimp-vc142-mtd.dll"      
+        $<TARGET_FILE_DIR:${PROJECT_NAME}>)
+
+    message(STATUS "Setting up copy yamlparser dll..." "${CMAKE_BINARY_DIR}/bin/Debug/")
+    add_custom_command(TARGET ${PROJECT_NAME}
+    POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different 
+        "${CMAKE_SOURCE_DIR}/application/configloader/dependencies/yamlparser/lib/msvc/yaml-cppd.dll"      
         $<TARGET_FILE_DIR:${PROJECT_NAME}>)
 endif()
